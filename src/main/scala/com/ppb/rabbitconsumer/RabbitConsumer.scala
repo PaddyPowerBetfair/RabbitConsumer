9db0b383f92890794f7fe58e9cadf68c0bbff10b
Rob O'Doherty <rodoherty1@gmail.com>
2017-10-06 09:19:17 +0100

    Another basic refactoring in prep for some unit tests

--- src/main/scala/com/ppb/rabbitconsumer/RabbitConsumer.scala
+++ src/main/scala/com/ppb/rabbitconsumer/RabbitConsumer.scala
@@ -1,20 +1,17 @@
 package com.ppb.rabbitconsumer
 
-
-import com.rabbitmq.client._
-
 import argonaut._
-import Argonaut._
 
 import scala.collection.JavaConverters._
 import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.LoggerFactory
 
 import scala.collection.immutable
+import scala.util.{Success, Try}
 import scalaz.concurrent.Task
 import scalaz.stream._
 
-case class Cxn(connection: Connection, channel: com.rabbitmq.client.Channel, queueName: String)
+case class Cxn(filename: String, nextMessage: () => Try[Json], disconnect: () => Try[Unit])
 
 object RabbitConsumer {
   val jsonPreamble = "{\n    \"all\": ["
@@ -41,39 +38,29 @@ object RabbitConsumer {
 
     val cxns = connections.map(ConnectionService.init)
 
-    connections zip cxns foreach {
-      case (config, cxn) =>
-        val filename = config.getString("fileName").replaceFirst("^~", System.getProperty("user.home"))
+      cxns foreach { cxn => {
+        getMessages(cxn).run.run
+        cxn.disconnect()
+      }}
 
-        getMessages(filename)(cxn).run.run
+      logger.info(s"Done receiving $configName messages")
 
-        ConnectionService.close(cxn)
-    }
-
-    logger.info(s"Done receiving $configName messages")
-
-    logger.info(s"""When you're done testing, run "R.done("$configName") to delete the following Rabbit queues:""")
-    connections.foreach { config =>
-      logger.info(s"- ${config.getString("queue")}")
-    }
+      logger.info(s"""When you're done testing, run "R.done("$configName") to delete the following Rabbit queues:""")
+      connections.foreach { config =>
+        logger.info(s"- ${config.getString("queue")}")
+      }
   }
 
-  private def getMessages(filename: String)(cxn: Cxn): Process[Task, Unit] = {
+  private def getMessages(cxn: Cxn): Process[Task, Unit] = {
     Process(jsonPreamble) ++
-      (receiveAll(cxn) map (_.spaces2) intersperse ",") ++
-      Process(jsonPostamble) pipe text.utf8Encode to io.fileChunkW(filename)
+      (receiveAll(cxn.nextMessage) map (_.spaces2) intersperse ",") ++
+      Process(jsonPostamble) pipe text.utf8Encode to io.fileChunkW(cxn.filename)
   }
 
-  private def receiveAll(cxn: Cxn): Process0[Json] = {
-    val response = Option(cxn.channel.basicGet(cxn.queueName, false))
-
-    val json = response.flatMap { res =>
-      new String(res.getBody, "UTF-8").parseOption
-    }
-
-    json match {
-      case Some(txt) => Process.emit(txt) ++ receiveAll(cxn)
-      case None      => Process.halt
+  private def receiveAll(nextMessage: () => Try[Json]): Process0[Json] = {
+    nextMessage() match {
+      case Success(txt) => Process.emit(txt) ++ receiveAll(nextMessage)
+      case _            => Process.halt
     }
   }
 }
