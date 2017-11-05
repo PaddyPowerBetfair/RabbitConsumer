ce1c7406bd590101e7529dead016d59bbc37c353
ainulhabib <ainul.habib@paddyposwebetfair.com>
2017-11-05 23:10:04 +0000

    updated code to add configuration file to docker to start exchange

--- src/it/scala/com/ppb/rabbitconsumer/RabbitConsumerITSpec.scala
+++ src/it/scala/com/ppb/rabbitconsumer/RabbitConsumerITSpec.scala
@@ -11,26 +11,8 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 class RabbitConsumerITSpec extends FlatSpec with Matchers {
 
-  private val logger = LoggerFactory.getLogger(RabbitConsumer.getClass)
 
   it should "read docker local configuration files and process it" in {
-    val filename: String = (RabbitConsumer.getConfigs("local")).configs(1).getString("fileName").replaceFirst("^~", System.getProperty("user.home"))
     RabbitConsumer.local
-    for (line <- Source.fromFile(filename).getLines) {
-          logger.info(line)
-     }
   }
-}
-
-trait RabbitConsumerFixture {
-
-  var times = 0
-
-  val receiveOneMessage: () => Try[Json] = () =>
-    if (times == 0) {
-      times = 1
-      Success("".asJson)
-    } else {
-      Failure(new Exception())
-    }
 }
